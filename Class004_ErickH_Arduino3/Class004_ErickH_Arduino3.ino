490353be4ac5d67f2e10804ec266a07b657965d4
Erick Hernandez <erickhb@gmail.com>
2016-06-20 16:26:55 -0600

    Add files via upload

---
+++ Class004_ErickH_Arduino3/Class004_ErickH_Arduino3.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+Servo myServo;
+int posX = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myServo.attach(2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(posX);
+  int val = map(posX,0,600,0,180);
+  myServo.write(val);
+  //delay(50);
+}
+
+void serialEvent()
+{
+  if(Serial.available())
+  {
+    posX = Serial.parseInt();
+  }
+}
+
